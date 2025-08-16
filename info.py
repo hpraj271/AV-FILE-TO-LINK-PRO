0ecebeacdab20808a7da5f8e8486f90f657aa98d
hpraj271 <hpraj271@gmail.com>
2025-08-16 20:00:53 +0530

    Update info.py
    Signed-off-by: hpraj271 <hpraj271@gmail.com>

--- info.py
+++ info.py
@@ -47,14 +47,14 @@ SHORTLINK_URL = environ.get('SHORTLINK_URL', '')  # Shortener site
 SHORTLINK_API = environ.get('SHORTLINK_API', '')  # API key for shortlink
 
 # 💾 MongoDB Connection Information
-DB_URL = environ.get('DATABASE_URI', "")  # MongoDB connection URI
-DB_NAME = environ.get('DATABASE_NAME', "")  # MongoDB database name
+DB_URL = environ.get('DATABASE_URI', "mongodb+srv://Hanuman:hanuman@cluster0.l5toshc.mongodb.net/?retryWrites=true&w=majority")  # MongoDB connection URI
+DB_NAME = environ.get('DATABASE_NAME', "Hanuman")  # MongoDB database name
 
 # 📸 all Media (Images)
 QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/6afb4093d5ec5c4176979.jpg')  # QR Code image
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")  # Verify success image
 AUTH_PICS = environ.get('AUTH_PICS', 'https://envs.sh/AwV.jpg')  # Auth step image
-PICS = environ.get('PICS', 'https://envs.sh/_pM.jpg')  # Default info image
+PICS = environ.get('PICS', 'https://graph.org/file/819dd1a293d5fea054e07-d509d6c5c97bab7fe9.jpg')  # Default info image
 FILE_PIC = environ.get('FILE_PIC', 'https://i.ibb.co/bj4My0bW/photo-2025-07-21-02-15-21-7529360175656861700.jpg') # file image 
 
 # 📝 File Captions
